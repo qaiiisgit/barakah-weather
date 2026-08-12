34b686f0971cb7524511a031cc28b1e756cfa48b
Mohammad Qais Khan <qaisphone2@gmail.com>
2026-08-12 14:15:06 +0530

    Auto refresh logic

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -36,5 +36,5 @@ export const useWeather = (location) => {
     return () => clearInterval(interval);
   }, [location, loadWeather]);
 
-  return { weather, loading, refresh: loadWeather, lastFetched };
+  return { weather, loading,  error, refresh: loadWeather, lastFetched };
 };
