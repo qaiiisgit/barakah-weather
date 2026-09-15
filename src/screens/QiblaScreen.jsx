a6b1c478948de326da928a745e9f68193fed7ed0
Mohammad Qais Khan <qaisphone2@gmail.com>
2026-09-15 10:28:54 +0530

    Qibla Screen

--- src/screens/QiblaScreen.jsx
+++ src/screens/QiblaScreen.jsx
@@ -1,12 +1,30 @@
+import React from "react";
+import { useQibla } from '../hooks/useQibla'
+import ErrorCard from "../components/ErrorCard";
+
+
+
 const QiblaScreen = ({ location, locationName }) => {
-  const {
-    qiblaDirection,
-    compassHeading,
-    arrowRotation,
-    loading,
-    error,
-    compassSupported,
-    compassPermission,
-    requestCompassPermission,
-    refresh,
-  } = useQibla(location);
+    const {
+        qiblaDirection,
+        compassHeading,
+        arrowRotation,
+        loading,
+        error,
+        compassSupported,
+        compassPermission,
+        requestCompassPermission,
+        refresh,
+    } = useQibla(location);
+
+    if (!location) {
+        return (
+            <div className="px-4 pt-4">
+                <ErrorCard type="permission" message="Please enable location access or search for a city to find the Qibla direction." />
+            </div>
+        );
+    }
+
+}
+
+export default QiblaScreen;
